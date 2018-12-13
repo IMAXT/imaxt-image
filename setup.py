c4fa672294c5a40d80856e853d3542fe33c174d2
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2018-12-13 17:33:36 +0000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.ast.cam.ac.uk/imaxt/imaxt-image',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
     python_requires=">=3.5",
     ext_modules=[Extension('tif_lzw',
