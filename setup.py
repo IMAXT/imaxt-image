b876690ea8a6a4af17980661a9ac880a6e252086
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2019-10-03 12:43:59 +0100

    Bump version: 0.10.2 → 0.11.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.ast.cam.ac.uk/imaxt/imaxt-image',
-    version='0.10.2',
+    version='0.11.0',
     zip_safe=False,
     python_requires='>=3.5',
     dependency_links=['https://imaxt.ast.cam.ac.uk/pip/imagecodecs-lite/'],
