5de4ff5228064661f71187ac95d14331c0fe43bc
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2021-03-05 12:01:55 +0000

    Formatting quotes change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
 requirements = [
@@ -18,39 +18,41 @@ requirements = [
 
 extras_requirements = {"pyviz": ["matplotlib", "holoviews", "datashader"]}
 
-test_requirements = ['coverage', 'pytest', 'pytest-cov', 'pytest-mock']
+setup_requirements = ["pytest-runner", "flake8"]
+
+test_requirements = ["coverage", "pytest", "pytest-cov", "pytest-mock"]
 
 setup(
-    author='IMAXT Team',
-    maintainer='Eduardo Gonzalez Solares',
-    maintainer_email='eglez@ast.cam.ac.uk',
+    author="IMAXT Team",
+    maintainer="Eduardo Gonzalez Solares",
+    maintainer_email="eglez@ast.cam.ac.uk",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    description='IMAXT Image Utilities',
+    description="IMAXT Image Utilities",
     install_requires=requirements,
-    license='GNU General Public License v3',
+    license="GNU General Public License v3",
     long_description=readme,
     include_package_data=True,
-    keywords='imaxt',
-    name='imaxt-image',
-    packages=find_packages(include=['imaxt*']),
+    keywords="imaxt",
+    name="imaxt-image",
+    packages=find_packages(include=["imaxt*"]),
     setup_requires=setup_requirements,
     extras_requires=extras_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url='https://gitlab.ast.cam.ac.uk/imaxt/imaxt-image',
-    version='0.15.5',
+    url="https://gitlab.ast.cam.ac.uk/imaxt/imaxt-image",
+    version="0.15.5",
     zip_safe=False,
-    python_requires='>=3.5',
+    python_requires=">=3.5",
     dependency_links=[
-        'https://imaxt.ast.cam.ac.uk/pip/imagecodecs-lite/',
-        'https://imaxt.ast.cam.ac.uk/pip/zarr',
+        "https://imaxt.ast.cam.ac.uk/pip/imagecodecs-lite/",
+        "https://imaxt.ast.cam.ac.uk/pip/zarr",
     ],
 )
