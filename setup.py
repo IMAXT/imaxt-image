1378f0478d0b2948266b564cca37f80155317b00
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2021-07-29 10:14:56 +0100

    Bump version: 0.17.0 → 0.17.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/IMAXT/imaxt-image",
-    version="0.17.0",
+    version="0.17.1",
     zip_safe=False,
     python_requires=">=3.7",
 )
