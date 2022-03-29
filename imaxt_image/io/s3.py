7b297cd06727be3345ca59abd5824995864b3949
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2022-03-29 14:49:08 +0000

    Allow listing contents of storage

--- imaxt_image/io/s3.py
+++ imaxt_image/io/s3.py
@@ -11,7 +11,31 @@ def get_mc_config(alias):
     return config["aliases"][alias]
 
 
-def get_s3_store(name, alias="imaxtgw"):
+def list_s3_store(alias="imaxtgw", instrument="stpt"):
+    config = get_mc_config(alias)
+    s3 = s3fs.S3FileSystem(
+        key=config["accessKey"],
+        secret=config["secretKey"],
+        client_kwargs={"endpoint_url": config["url"]},
+    )
+    s3list = []
+    for ppath in [
+        "processed",
+        "processed0",
+        "processed1",
+        "processed2",
+        "processed3",
+        "processed4",
+    ]:
+        try:
+            this = f"{ppath}/{instrument}"
+            s3list = s3list + [a.replace(this+'/', '') for a in s3.ls(this)]
+        except PermissionError:
+            pass
+    return s3list
+
+
+def get_s3_store(name, alias="imaxtgw", instrument="stpt", extra="mos.zarr"):
     config = get_mc_config(alias)
     s3 = s3fs.S3FileSystem(
         key=config["accessKey"],
@@ -27,7 +51,7 @@ def get_s3_store(name, alias="imaxtgw"):
         "processed3",
         "processed4",
     ]:
-        s3path = f"{ppath}/stpt/{name}/mos.zarr"
+        s3path = f"{ppath}/{instrument}/{name}/{extra}"
         if s3.exists(s3path):
             s3store = s3.get_mapper(s3path)
             break
